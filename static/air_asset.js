8599a2b97e1437d0929248b42acec1b9b11a6693
Exekutiveone <110990173+Exekutiveone@users.noreply.github.com>
2025-07-12 19:11:36 +0200

    Implement target assignment and pathfinding

--- static/air_asset.js
+++ static/air_asset.js
@@ -3,19 +3,50 @@ let airInterval = null;
 export function initAirAsset(map) {
   const icon = L.icon({
     iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png',
-    iconSize: [32, 32]
+    iconSize: [32, 32],
+    className: 'unit-icon'
   });
-  const marker = L.marker([48.7758, 9.1829], { icon }).addTo(map).bindPopup("Fluggerät");
-  return marker;
+  const marker = L.marker([48.7758, 9.1829], { icon }).addTo(map).bindPopup('Fluggerät');
+  const asset = {
+    marker,
+    interval: null,
+    pathLine: null,
+    target: null,
+    targetMarker: null,
+    isAir: true,
+    moveTo: (target) => moveAirTo(marker, target, asset),
+    onSelect: null,
+    onDragEnd: null,
+  };
+
+  marker.on('click', () => {
+    if (asset.onSelect) asset.onSelect(asset);
+  });
+
+  return asset;
 }
 
-export function moveAirTo(marker, target) {
+export function moveAirTo(marker, target, asset) {
   if (airInterval) clearInterval(airInterval);
+
+  const map = marker._map;
+  if (asset.pathLine) {
+    map.removeLayer(asset.pathLine);
+  }
+
+  asset.target = target;
+  asset.pathLine = L.polyline([marker.getLatLng(), target], { color: 'red' }).addTo(map);
 
   airInterval = setInterval(() => {
     const next = moveTowards(marker.getLatLng(), target, 0.0012);
     marker.setLatLng(next);
-    if (next.lat === target.lat && next.lng === target.lng) clearInterval(airInterval);
+    if (next.lat === target.lat && next.lng === target.lng) {
+      clearInterval(airInterval);
+      if (asset.pathLine) {
+        map.removeLayer(asset.pathLine);
+        asset.pathLine = null;
+      }
+    }
   }, 100);
 }
 
--- static/ground_asset.js
+++ static/ground_asset.js
@@ -1,16 +1,35 @@
 export function createGroundAsset(map, position) {
   const icon = L.icon({
     iconUrl: 'https://cdn-icons-png.flaticon.com/512/148/148767.png',
-    iconSize: [32, 32]
+    iconSize: [32, 32],
+    className: 'unit-icon'
   });
 
-  const marker = L.marker(position, { icon }).addTo(map).bindPopup("Bodenfahrzeug");
+  const marker = L.marker(position, { icon, draggable: true }).addTo(map).bindPopup('Bodenfahrzeug');
 
   const asset = {
     marker,
     interval: null,
+    pathLine: null,
+    target: null,
+    targetMarker: null,
+    isAir: false,
+    onSelect: null,
+    onDragEnd: null,
     moveTo: (target) => moveGroundTo(marker, target, asset)
   };
+
+  marker.on('click', () => {
+    if (asset.onSelect) asset.onSelect(asset);
+  });
+
+  marker.on('dragstart', () => {
+    if (asset.interval) clearInterval(asset.interval);
+  });
+
+  marker.on('dragend', () => {
+    if (asset.onDragEnd) asset.onDragEnd(asset);
+  });
 
   return asset;
 }
@@ -21,6 +40,12 @@ function moveGroundTo(marker, target, asset) {
 
   const map = marker._map;
 
+  if (asset.pathLine) {
+    map.removeLayer(asset.pathLine);
+  }
+
+  asset.target = target;
+
   const control = L.Routing.control({
     waypoints: [marker.getLatLng(), target],
     createMarker: () => null,
@@ -29,10 +54,15 @@ function moveGroundTo(marker, target, asset) {
   }).on('routesfound', function (e) {
     map.removeControl(control);  // sauber entfernen
     const coords = e.routes[0].coordinates;
+    asset.pathLine = L.polyline(coords, { color: 'blue' }).addTo(map);
     let i = 0;
     asset.interval = setInterval(() => {
       if (i >= coords.length) {
         clearInterval(asset.interval);
+        if (asset.pathLine) {
+          map.removeLayer(asset.pathLine);
+          asset.pathLine = null;
+        }
         return;
       }
       marker.setLatLng(coords[i]);
